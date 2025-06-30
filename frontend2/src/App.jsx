2e6786b1fdefe4de7d5a59f72847bc41d52aa6c6
lakshmanBaskaran <70500931+lakshmanBaskaran@users.noreply.github.com>
2025-06-30 13:43:19 +0100

    Initial commit of NASA API fullstack project

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import CssBaseline from '@mui/material/CssBaseline';
 
-import Layout from './components/layout/Layout.jsx';
+import Layout from './components/layout/layout.jsx';
 import HomePage from './pages/HomePage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
 
