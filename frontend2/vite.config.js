77412f614f2cfd6972e27eb60eabe4c0589bf467
lakshmanBaskaran <70500931+lakshmanBaskaran@users.noreply.github.com>
2025-06-30 14:34:31 +0100

    Initial commit of NASA API fullstack project

--- frontend2/vite.config.js
+++ frontend2/vite.config.js
@@ -1,19 +1,21 @@
-// vite.config.js
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      // any request to /api/** will be forwarded to localhost:8080
-      '/api': {
-        target: ''http://localhost:8080'',
-        changeOrigin: true,
-        //rewrite: (path) => path.replace(/^\/api/, ''), 
-        // so /api/apod?date=... → http://localhost:8080/apod?date=...
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  // Load .env variables based on the mode (e.g., development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8080', // fallback for dev
+          changeOrigin: true,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
+        }
+      }
+    }
+  };
 });
